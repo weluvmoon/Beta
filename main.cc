b510462c155dbdcb78f2a70e03532b5cdb88f285
weluvmoon <doc.demsey@gmail.com>
2025-12-29 08:20:33 +0100

    new file:   main.cc new file:   src/game.cc

---
+++ main.cc
@@ -0,0 +1,38 @@
+#include "raylib.h"
+
+#include "src/modules/entities.h"
+#include "src/modules/game.h"
+
+int main() {
+  const int screenWidth = 640;
+  const int screenHeight = 420;
+  const int FrameCap = 60;
+
+  InitWindow(screenWidth, screenHeight, "Beta");
+  SetTargetFPS(FrameCap);
+
+  // Initialize the container structure
+
+  Game game;
+  game.Init();
+
+  while (!WindowShouldClose()) {
+    game.Update(GetFrameTime());
+
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    game.Draw();
+
+    // You can add general UI elements or collision text here
+    DrawFPS(10, 10);
+
+    EndDrawing();
+  }
+
+  game.Unload();
+
+  CloseWindow();
+  return 0;
+}
